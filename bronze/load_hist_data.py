6d863f5d2fd094810e4d3b8d56be2e030224c7e3
dineshmote <47915123+dineshmote@users.noreply.github.com>
2024-10-28 17:24:02 +0530

    parquet read write

--- bronze/load_hist_data.py
+++ bronze/load_hist_data.py
@@ -5,8 +5,10 @@ import requests
 from datetime import datetime
 from google.cloud import storage
 import logging
+from apache_beam.io.parquetio import WriteToParquet, ReadFromParquet
 import os 
 from apache_beam.io.gcp.bigquery import WriteToBigQuery, BigQueryDisposition
+import pyarrow as pa
 
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Dinesh Mote\Downloads\gcp-data-project-433112-aecffc0dc374.json"
@@ -24,6 +26,12 @@ class FetchDataFromAPI(beam.DoFn):
         else:
             raise Exception(f"Failed to fetch data from API: {response.status_code}")
 
+def log_data(record):
+    logging.info("-----------------------------------------------")
+    logging.info(record)
+    logging.info("-----------------------------------------------")
+
+    return record
 
 def convert_timestamp_to_gmt(timestamp_ms):
     """
@@ -47,26 +55,26 @@ class FlattenJSONData(beam.DoFn):
             coordinates = geometry["coordinates"]
 
             flattened_record = {
-                "place": properties.get("place"),
+                "place": str(properties.get("place")),
                 "mag": float(properties.get("mag")) if properties.get("mag") is not None else None,
                 "time": convert_timestamp_to_gmt(properties.get("time")),
                 "updated": convert_timestamp_to_gmt(properties.get("updated")),
-                "tz": properties.get("tz"),
+                "tz": int(properties.get("tz")) if properties.get("tz") is not None else None,  # Ensure this is an integer
                 "url": properties.get("url"),
                 "detail": properties.get("detail"),
-                "felt": properties.get("felt"),
+                "felt": int(properties.get("felt")) if properties.get("felt") is not None else None,
                 "cdi": float(properties.get("cdi")) if properties.get("cdi") is not None else None,
                 "mmi": float(properties.get("mmi")) if properties.get("mmi") is not None else None,
                 "alert": properties.get("alert"),
                 "status": properties.get("status"),
-                "tsunami": properties.get("tsunami"),
-                "sig": properties.get("sig"),
+                "tsunami": int(properties.get("tsunami")) if properties.get("tsunami") is not None else None,
+                "sig": int(properties.get("sig")) if properties.get("sig") is not None else None,
                 "net": properties.get("net"),
                 "code": properties.get("code"),
                 "ids": properties.get("ids"),
                 "sources": properties.get("sources"),
                 "types": properties.get("types"),
-                "nst": properties.get("nst"),
+                "nst": int(properties.get("nst")) if properties.get("nst") is not None else None,
                 "dmin": float(properties.get("dmin")) if properties.get("dmin") is not None else None,
                 "rms": float(properties.get("rms")) if properties.get("rms") is not None else None,
                 "gap": float(properties.get("gap")) if properties.get("gap") is not None else None,
@@ -115,8 +123,6 @@ def run():
     google_cloud_options.temp_location = 'gs://earthquake_analysis_data_bucket/stage_loc'
     google_cloud_options.staging_location = 'gs://earthquake_analysis_data_bucket/temp_loc'
     
-    # options.view_as(StandardOptions).runner = 'DataflowRunner'  # Use 'DirectRunner' for local execution
-    
     worker_options = options.view_as(WorkerOptions)
     worker_options.machine_type = 'n1-standard-4'
     
@@ -127,32 +133,105 @@ def run():
     
     bronze_output_path = f"gs://{bucket_name}/beam/landing/{current_date}/earthquake_raw"
     silver_output_path = f"gs://{bucket_name}/beam/silver/{current_date}/earthquake_transformed"
+    parquet_output_path = f"gs://{bucket_name}/beam/silver/{current_date}/earthquake_transformed"
     
     table_spec = 'gcp-data-project-433112:earthquake_ingestion.earthquake_table_dataflow'
     
+    # Define the table schema before it's used
+    table_schema = {
+        "fields": [
+            {"name": "place", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "mag", "type": "FLOAT", "mode": "NULLABLE"},
+            {"name": "time", "type": "TIMESTAMP", "mode": "NULLABLE"},
+            {"name": "updated", "type": "TIMESTAMP", "mode": "NULLABLE"},
+            {"name": "tz", "type": "INTEGER", "mode": "NULLABLE"},
+            {"name": "url", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "detail", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "felt", "type": "INTEGER", "mode": "NULLABLE"},
+            {"name": "cdi", "type": "FLOAT", "mode": "NULLABLE"},
+            {"name": "mmi", "type": "FLOAT", "mode": "NULLABLE"},
+            {"name": "alert", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "status", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "tsunami", "type": "INTEGER", "mode": "NULLABLE"},
+            {"name": "sig", "type": "INTEGER", "mode": "NULLABLE"},
+            {"name": "net", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "code", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "ids", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "sources", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "types", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "nst", "type": "INTEGER", "mode": "NULLABLE"},
+            {"name": "dmin", "type": "FLOAT", "mode": "NULLABLE"},
+            {"name": "rms", "type": "FLOAT", "mode": "NULLABLE"},
+            {"name": "gap", "type": "FLOAT", "mode": "NULLABLE"},
+            {"name": "magType", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "type", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "title", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "geometry", "type": "RECORD", "mode": "NULLABLE", "fields": [
+                {"name": "longitude", "type": "FLOAT", "mode": "NULLABLE"},
+                {"name": "latitude", "type": "FLOAT", "mode": "NULLABLE"},
+                {"name": "depth", "type": "FLOAT", "mode": "NULLABLE"}
+            ]},
+            {"name": "area", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "insert_date", "type": "DATE", "mode": "NULLABLE"}
+        ]
+    }
+    
+    # Define the Apache Arrow schema for Parquet
+    arrow_schema = pa.schema([
+        ("place", pa.string()),
+        ("mag", pa.float32()),
+        ("time", pa.string()),
+        ("updated", pa.string()),
+        ("tz", pa.int32()),
+        ("url", pa.string()),
+        ("detail", pa.string()),
+        ("felt", pa.int32()),
+        ("cdi", pa.float32()),
+        ("mmi", pa.float32()),
+        ("alert", pa.string()),
+        ("status", pa.string()),
+        ("tsunami", pa.int32()),
+        ("sig", pa.int32()),
+        ("net", pa.string()),
+        ("code", pa.string()),
+        ("ids", pa.string()),
+        ("sources", pa.string()),
+        ("types", pa.string()),
+        ("nst", pa.int32()),
+        ("dmin", pa.float32()),
+        ("rms", pa.float32()),
+        ("gap", pa.float32()),
+        ("magType", pa.string()),
+        ("type", pa.string()),
+        ("title", pa.string()),
+        ("geometry", pa.struct([
+            ("longitude", pa.float32()),
+            ("latitude", pa.float32()),
+            ("depth", pa.float32())
+        ])),
+        ("area", pa.string())
+        ])
+        
     with beam.Pipeline(options=options) as p:
         # Fetch data from API and write raw data to GCS
         raw_data = (p
                     | 'Create API URL' >> beam.Create([api_url])
                     | 'Fetch Data from API' >> beam.ParDo(FetchDataFromAPI())
-                    #  | 'Log Raw Data' >> beam.Map(lambda x: logging.info(f"Raw data fetched: {x}"))
                    )
         
         # Write raw data to GCS
-        write_raw_data_to_gcs = ( raw_data
+        write_raw_data_to_gcs = (raw_data
                                  | "Format To JSON" >> beam.Map(lambda x: json.dumps(x))
                                  | "Write Raw Data to GCS" >> beam.io.WriteToText(bronze_output_path, 
                                     file_name_suffix=".json", 
                                     shard_name_template=""
                                     )
-                                #   | "Log Raw Data Write" >> beam.Map(lambda x: logging.info(f"Raw data written to GCS: {bronze_output_path}"))
                                )
         
         # Read raw data from GCS
         raw_data_from_gcs = (write_raw_data_to_gcs
                              | 'Read Raw Data from GCS' >> beam.io.ReadFromText(bronze_output_path +".*json")
                              | 'Parse JSON' >> beam.Map(json.loads)
-                            #  | 'Log Read Data' >> beam.Map(lambda x: logging.info(f"Raw data read from GCS: {x}"))
                             )
 
         # Transform and flatten the data
@@ -164,58 +243,22 @@ def run():
         transformed_with_area = (transformed_data
                                  | 'Add Column Area' >> beam.ParDo(AddColumnArea())
                                 )
-
         
-        # Add insert_date to each record
-        transformed_with_insert_date = (transformed_with_area
-                                        | 'Add Insert Date' >> beam.ParDo(AddInsertDate())
-                                       )
-        
-        # Write transformed data to GCS
-        (transformed_with_insert_date
-         | 'Write Transformed Data to GCS' >> beam.io.WriteToText(silver_output_path, 
-                                                                   file_name_suffix=".json", 
-                                                                   shard_name_template="", 
-                                                                   num_shards=1)
+        # Write transformed data to Parquet before adding insert date
+        transformed_with_area | 'Write to Parquet' >> WriteToParquet(
+            file_path_prefix=parquet_output_path,
+            schema=arrow_schema,
+            file_name_suffix=".parquet"
         )
         
-        table_schema = {
-        "fields": [
-                    {"name": "place", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "mag", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "time", "type": "TIMESTAMP", "mode": "NULLABLE"},
-                    {"name": "updated", "type": "TIMESTAMP", "mode": "NULLABLE"},
-                    {"name": "tz", "type": "INTEGER", "mode": "NULLABLE"},
-                    {"name": "url", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "detail", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "felt", "type": "INTEGER", "mode": "NULLABLE"},
-                    {"name": "cdi", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "mmi", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "alert", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "status", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "tsunami", "type": "INTEGER", "mode": "NULLABLE"},
-                    {"name": "sig", "type": "INTEGER", "mode": "NULLABLE"},
-                    {"name": "net", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "code", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "ids", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "sources", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "types", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "nst", "type": "INTEGER", "mode": "NULLABLE"},
-                    {"name": "dmin", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "rms", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "gap", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "magType", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "type", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "title", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "geometry", "type": "RECORD", "mode": "NULLABLE", "fields": [
-                        {"name": "longitude", "type": "FLOAT", "mode": "NULLABLE"},
-                        {"name": "latitude", "type": "FLOAT", "mode": "NULLABLE"},
-                        {"name": "depth", "type": "FLOAT", "mode": "NULLABLE"}
-                    ]},
-                    {"name": "area", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "insert_date", "type": "DATE", "mode": "NULLABLE"}
-                ]
-            }
+        # Read the Parquet file back for the insert date operation
+        transformed_with_insert_date = (p
+                                        | 'Read Transformed Data from Parquet' >> ReadFromParquet(parquet_output_path + "*.parquet")
+                                        | 'Add Insert Date' >> beam.ParDo(AddInsertDate())
+                                        |'Log Data' >> beam.Map(log_data)
+                                       )
+        
+        
         
         # Write transformed data to BigQuery
         transformed_with_insert_date | 'Write to BigQuery' >> beam.io.WriteToBigQuery(
@@ -224,6 +267,7 @@ def run():
             write_disposition=BigQueryDisposition.WRITE_APPEND,
             create_disposition=BigQueryDisposition.CREATE_IF_NEEDED
         )
+
 
 
 if __name__ == "__main__":
